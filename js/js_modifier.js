56bca9e1056bf679cc9e80aad963882a295d20da
MiaaSarre <35997342+MiaaSarre@users.noreply.github.com>
2018-03-01 22:22:10 +0100

    PageModif
    Les différentes pages ne switchent pas

---
+++ js/js_modifier.js
@@ -0,0 +1,34 @@
+function affEtabModif()
+{
+
+    if (document.getElementById('rbEtablissement').checked == true)
+    {
+        document.getElementById('modifierEtablissement').style.display = "";
+        document.getElementById('modifierEleve').style.display = "none";
+        document.getElementById('ajouterAVS').style.display = "none";
+        
+    } 
+    else if (document.getElementById('rbEleve').checked == true)
+    {
+       document.getElementById('modifierEtablissement').style.display = "none";
+       document.getElementById('modifierEleve').style.display = "";
+       document.getElementById('modifierAVS').style.display = "none";   
+
+   }
+   else if (document.getElementById('rbAVS').checked == true)
+   {
+       document.getElementById('modifierEtablissement').style.display = "none";
+       document.getElementById('modifierEleve').style.display = "none";
+       document.getElementById('modifierAVS').style.display = "";
+
+   }
+
+    
+
+}
+
+
+
+
+
+
--- vues/v_modifier.php
+++ vues/v_modifier.php
@@ -1,5 +1,4 @@
 
-
 <div class="container">
     
    
@@ -17,15 +16,16 @@
          <div class ="col">   
          </div>
              <div class ="col">
-        <input  type="radio" name="rb" id="rbEtablissement" checked="true" onclick="affEtab();" />
+        <input  type="radio" name="rb" id="rbEtablissement" checked="true" onclick="affEtabModif();" />
         <label class="radio"  for="rbEtablissement">Etablissement</label>   
-        <input   type="radio" name="rb" id="rbEleve" onclick="affEtab();"/>
+        <input   type="radio" name="rb" id="rbEleve" onclick="affEtabModif();"/>
         <label class="radio"for="rbEleve">Eleve</label>     
-        <input type="radio" name="rb" id="rbAVS" onclick="affEtab();" />
+        <input type="radio" name="rb" id="rbAVS" onclick="affEtabModif();" />
         <label class="radio"  for="rbAVS">AVS</label>
           </div>
          <div class="col">
          </div>
+         
      </div>
   
      <div class="row">   
@@ -45,49 +45,114 @@
               </select>
               <input type="submit" value="Supprimer" name="supprimerAVS" />
          </div>
-          <div class ="col">
-              <button type="submit" class="btn btn-default">Submit</button>
-  
-        </div>
+         <div class="col"></div>
+       
          
          <div class="col">
+            
+           <div class ="row">  
+               <div class ="col"
              <form name="modifierEtablissement"  id="modifierEtablissement"  action="index.php?do=modifier&action=mofifierEtablissement" method="POST">
-       <div class="form-control"
-        <label for="nomEtablissement">Nom:</label>
-        <input type="text" name="nomEtablissement" value="" required/>
-        <br>
-       </div>
-         <div class="form-control"
-        <label for="typeEtablissement">Type établissement:</label>
-        <input class="form-control" type="text" name="typeEtablissement" value="" required />
-        <br>
+                 
+                 <div class="form"
+                    <label for="nomEtablissement"> Nom:</label>
+                  </div>  
+                 </div>
+               <div class="col">  
+                    <input type="text" name="nomEtablissement" value="" required/>
+                    <br>    
+               </div>    
+               </div>
+              
+           <div class="row">
+            <div class="col">
+               <div class="form"
+               <label for="typeEtablissement">Type établissement:</label>
+               </div>
+            </div> 
+            <div class="col">
+                <input class="form-control" type="text" name="typeEtablissement" value="" required />
+                <br>
+            </div>
          </div>
-         <div class="form-control"
-        <label for="responsableEtablissement">Responsable:</label>
-        <input type="text" name="responsableEtablissement" value="" required />
-        <br>
+             
+         <div class="row">   
+        <div class="col">
+            <div class="form"
+           <label for="responsableEtablissement">Responsable:</label>
          </div>
+        </div>   
+         <div class="col">
+            <input type="text" name="responsableEtablissement" value="" required />
+            <br>
+             </div>
+         </div>
+        
         <input type="submit" value="Modifier" name="modifierEtab" />
     </form>
-
-    <form name="modifierEleve" id="modifierEleve" style="display: none;" action="index.php?do=modifier&action=modifierEleve" method="POST">
-        <div class="form-control"
+           </div>              
+ 
+     <div class ="col">
+           <form class="navbar-form navbar-left" role="search">
+            <div class="form-group">
+              <input type="text" class="form-control" placeholder="Search">
+            </div>
+            
+          </form>
+             <select name="Eleve[]" multiple>
+              <?php
+            for ($i = 0; $i < sizeof($listEleve); $i++) {
+                echo'<option id='.$listEleve[$i][id_eleve].' value='.$listEleve[$i][id_etablissement].'>' . $listEleve[$i][nom].'</option>';
+            }
+            ?>
+              </select>
+              <input type="submit" value="Supprimer" name="supprimerEleve" />
+     </div>   
+      <div class="col">  </div>
+                   
+     <div class="col">       
+     <div class="row">
+         <div class="col">
+        <form name="modifierEleve" id="modifierEleve" style="display: none;" action="index.php?do=modifier&action=modifierEleve" method="POST">
+        <div class="form"
         <label for="nomEleve">Nom:</label>
+        </div>  
+        </div>        
+        <div class="col">        
         <input type="text" name="nomEleve" value="" required />
         <br>
         </div>
-        <div class="form-control"
-        <label for="prenomEleve">Prénom:</label>
-        <input type="text" name="prenomEleve" value="" required />
-        <br>
-        </div>
-        <div class="form-control"
+     </div>
+
+        <div class="row">  
+            <div class="col">
+                <div class="form"
+                <label for="prenomEleve">Prénom:</label>
+                </div>
+            </div>
+                <div class="col">
+                <input type="text" name="prenomEleve" value="" required />
+                <br>
+                </div>
+            </div>
+       
+         
+    <div class="row">
+        <div class="col">
+        <div class="form"
         <label for="dateNaissanceEleve">Date de naissance:</label>
         <br>
+        </div>    
+        <div class="col">
         <input type="date" name="dateNaissanceEleve" value="" required/>
         <br>
+        </div>    
         </div>
-        <div class="form-control"
+     </div>
+         
+         <div class="row">
+             <div class="col">
+        <div class="form"
         <label for="etablissementEleve">Etablissement:</label>
         <select name="etablissementEleve" required >
             <option>Choisir un établissement</option>
@@ -99,39 +164,99 @@
         </select>
         <br>
         </div>
-        <div class="form-control"
-        <label for="classeEleve">Classe:</label>
-        <input type="text" name="classeEleve" value="" required />
-        <br>
-        </div>
-
-        <input type="submit" value="Sauvegarder" name="envoyerEleve"/>
+        </div>    
+         </div> 
+         <div class="row">
+            <div class="col">
+           <div class="form"
+           <label for="classeEleve">Classe:</label>
+           </div>
+            </div>     
+           <div class="col">
+           <input type="text" name="classeEleve" value="" required />
+           <br>
+           </div>
+        </div>     
+        <input type="submit" value="Supprimer" name="envoyerEleve"/>
     </form>
+ </div>  
+              
 
+ <div class ="col">
+           <form class="navbar-form navbar-left" role="search">
+            <div class="form-group">
+              <input type="text" class="form-control" placeholder="Search">
+            </div>
+            
+          </form>
+             <select name="AVS[]" multiple>
+              <?php
+            for ($i = 0; $i < sizeof($listAvs); $i++) {
+                echo'<option id='.$listAvs[$i][id_eleve].' value='.$listAvs[$i][id_etablissement].'>' . $listAvs[$i][nom].'</option>';
+            }
+            ?>
+              </select>
+              <input type="submit" value="Supprimer" name="supprimerAvs" />
+     </div>   
+      <div class="col">  </div>
+      
+      <div class="col">
+          
+     <div class="row">
+       <div class="col">
     <form name="modifierAVS" id="modifierAVS" style="display: none;" action="index.php?do=modifier&action=modifierAVS" method="POST">
-        <div class="form-control"
+        <div class="form"
         <label for="nomAVS">Nom:</label>
+         </div>  
+       </div>
+         <div class="col">
         <input type="text" name="nomAVS" value="" required />
         <br>
         </div>
-        <div class="form-control"
+     </div>
+     
+       <div class="row">
+       <div class="col">
+        <div class="form"
         <label for="PrenomAVS">Prénom:</label>
+        </div>
+       </div>
+           <div class="col">   
         <input type="text" name="PrenomAVS" value="" required />
         <br>
         </div>
-        <div class="form-control"
+       </div>
+          
+        <div class="row">
+       <div class="col">
+        <div class="form"
         <label for="dateNaissanceAVS">Date de naissance:</label>
+        </div>
+       </div>
+         <div class="col">
         <input type="date" name="dateNaissanceAVS" value="" required />
         <br>
         </div>
-        <div class="form-control"
+        </div>
+          
+        <div class="row">
+       <div class="col">
+        <div class="form"
         <label for="emailAVS">Email:</label>
+        </div>
+       </div>
+        <div class="col">
         <input type="email" name="emailAVS" value="" placeholder="xyz@gmail.com" required />
         <br>
         </div>
-        <div class="form-control"
+      </div>
+     
+       <div class="row">
+       <div class="col"> 
+        <div class="form"
         <select name="eleveAssigneAVS[]" multiple>
               <?php
+              $listEleve= array ();
             for ($i = 0; $i < sizeof($listEleve); $i++) {
                 echo'<option id='.$listEleve[$i][id_eleve].' value='.$listEleve[$i][id_eleve].'>' . $listEleve[$i][nom] ." ". $listEleve[$i][prenom] . '</option>';
             }
@@ -140,11 +265,20 @@
         <br>
         <br>
         </div>
+       </div>
+           <div class="col">
         <input type="submit" value="Modifier" name="modifierAVS" />
-    </form> 
+        </div>
          </div>
+          </div> 
+    </form> 
+         
+     
+        
+               
 </div>
 </div>
+
 
 </body>
 </html>
