7cbe428b8854a6c0b79fd06a9f8f4b7d3feedcb9
MiaaSarre <35997342+MiaaSarre@users.noreply.github.com>
2018-03-13 18:28:41 +0100

    HistoriqueBootstrap

--- vues/v_historique.php
+++ vues/v_historique.php
@@ -1,17 +1,20 @@
 
 <div class="container">
     <div class="row">
-        <div class="col-xl-12" style="margin-top: 20px;">
-            <center>
+        <div class="col"></div>
+        <div class="col">
+        
                 <h2>Historique</h2>
-            </center>
         </div>
-    </div>
-</div>
-<div class="w-100"></div>
-<div class="container">
+        <div class="col"></div>
+        </div>
+   
+ 
+
+
     <div class="row">
-        <div class="col-xl-12" style="padding-left: 120px;padding-top: 20px;">
+       
+        <div class="col">
             <div class="searchbar"> <!--BARRE DE RECHERCHE-->
                 <div class="container-1"> <!--CONTAINER-->
                     <span class="icon"><img src=https://cdn1.iconfinder.com/data/icons/hawcons/32/698627-icon-111-search-512.png class="fa fa-search" width=32></span> <!--ICONE DE RECHERCHETITRE-->
@@ -21,100 +24,97 @@
             </div>
 
         </div>
+         <div class="col"></div>
+        <div class="col"></div>
     </div>
-</div>
-<div class="container">
+
     <div class="row">
         <!--
         
         Colonne numero 1
         
         -->
-        <div class="col" id="ibAVS" name="col-AVS" style="margin-left: 115px;">
-            <fieldset>
-                <legend>
-
-                </legend>
-                <select class="col-ETAB" name="infoEtablissement" id="" size="20" style="margin-top: 20px">
+        
+        <div class="col">
+           
+               <p class="h3"> Annee
+               </p>
+        
+                <select class="col-ETAB" name="infoEtablissement" id="" multiple size="20">
                     <?php
                     foreach ($listAVS as $AVS) {
                         echo'<option id=' . $AVS['id'] . '>' . $AVS['nom'] . '</option>';
                     }
                     ?>
                 </select>
-            </fieldset>
+       
         </div>
+          <div class="col">   
+              <span class="icon"><img src=http://constellations.pagesperso-orange.fr/Mes%20images/fl28.gif class="fa fa-search" width=60></span>
+          </div>
+            
+        <div class="col"></div>
         <!--
        
-       
-       
-        -->
-        <div class="col" id="ibAVS" name="col-AVS" style="max-width: 230px;">
-            <fieldset>
-                <legend>
-                    <div style="padding-top: 200px;">
-                        <span class="icon"><img src=http://constellations.pagesperso-orange.fr/Mes%20images/fl28.gif class="fa fa-search" width=60></span>
-                    </div>     
-                </legend>
-
-            </fieldset>
-        </div>
-        <!--
         
         Colonne numero 2 AVS 
         
         --> 
         <div class="col" name="col-AVS" >
-            <fieldset style="margin-top: 15px;">
-                <legend>
-                    <H3 style="margin-left: 80px;">AVS</H3>
-                </legend>
-                <select class="col-ETAB" name="historiqueavs" id="" size="17" style="margin-top: 20px">
+        
+            <p class="h3"> 
+                AVS
+            </p>
+                <select class="col-ETAB" name="historiqueavs" id="" multiple size="20">
                     <?php
                     foreach ($listAVS as $AVS) {
                         echo'<option id=' . $AVS['id'] . '>' . $AVS['nom'] . '</option>';
                     }
                     ?>
                 </select>
-            </fieldset>
+           
         </div>
         <!--
        
       
        
         -->
-        <div class="col" id="ibAVS" name="col-AVS" style="max-width: 200px;">
-            <fieldset>
+       
+        <div class="col" id="ibAVS" name="col-AVS">
+           
                 <legend>
-                     <div style="padding-top: 200px;">
-                        <span class="icon"><img src=http://constellations.pagesperso-orange.fr/Mes%20images/fl28.gif class="fa fa-search" width=60></span>
-                    </div>   
-
+                        <span class="icon"><img src=http://constellations.pagesperso-orange.fr/Mes%20images/fl28.gif class="fa fa-search" width=60 alt="Responsive image"></span>
+                   
                 </legend>
-
-            </fieldset>
         </div>
+        <div class="col"></div>
         <!--
         
         Colonne numero 3
         
         -->
-        <div class="col" id="ibAVS" name="col-AVS" style="">
-            <fieldset style="margin-top: 15px;">
-                <legend>
-                    <H3 style="margin-left: 80px;">Eléve</H3>
-                </legend>
-                <select class="col-ETAB" name="infoEtablissement" id="" size="17" style="margin-top: 20px">
+        <div class="col" id="ibAVS" name="col-AVS">
+           
+                <p class="h3"> 
+                    Eleve
+               </p>
+                <select class="col-ETAB" name="infoEtablissement" id="" multiple size="20">
                     <?php
                     foreach ($listAVS as $AVS) {
                         echo'<option id=' . $AVS['id'] . '>' . $AVS['nom'] . '</option>';
                     }
                     ?>
                 </select>
-            </fieldset>
+          
         </div>
+         <div class="col"></div>
+          <div class="col"></div>
     </div>
+        
 </div>
+
+
+
 
 
 
