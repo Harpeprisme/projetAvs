6eaca6c077995d3b6db2b51c8eac7ea4950bc5aa
MiaaSarre <35997342+MiaaSarre@users.noreply.github.com>
2018-03-19 12:12:58 +0100

    MiseàJour paje Ajouter

--- vues/v_ajouter.php
+++ vues/v_ajouter.php
@@ -31,19 +31,20 @@
     <div class="row">   
         <div class ="col"></div>
         
+        <!--Page Etablissement -->
         <div class ="col">
             <form name="ajouterEtablissement"  id="ajouterEtablissement"  action="index.php?do=ajouter&action=ajouterEtablissement" method="POST">
-                <div class="form-control">
+                <div class="form">
                     <label for="nomEtablissement">Nom:</label>
                     <input type="text" name="nomEtablissement" value="" required/>
                     <br>
                 </div>
-                <div class="form-control">
+                <div class="form">
                     <label for="typeEtablissement">Type établissement:</label>
                     <input class="form-control" type="text" name="typeEtablissement" value="" required />
                     <br>
                 </div>
-                <div class="form-control">
+                <div class="form">
                     <label for="responsableEtablissement">Responsable:</label>
                     <input type="text" name="responsableEtablissement" value="" required />
                     <br>
@@ -54,25 +55,27 @@
                     </div>
                 </div>
             </form>
-
+            <!-- Fin ajout etablissement -->
+            
+             <!--Page Eleve -->
             <form name="ajouterEleve" id="ajouterEleve" style="display: none;" action="index.php?do=ajouter&action=ajouterEleve" method="POST">
-                <div class="form-control">
+                <div class="form">
                     <label for="nomEleve">Nom:</label>
                     <input type="text" name="nomEleve" value="" required />
                     <br>
                 </div>
-                <div class="form-control">
+                <div class="form">
                     <label for="prenomEleve">Prénom:</label>
                     <input type="text" name="prenomEleve" value="" required />
                     <br>
                 </div>
-                <div class="form-control">
+                <div class="form">
                     <label for="dateNaissanceEleve">Date de naissance:</label>
                     <br>
                     <input type="date" name="dateNaissanceEleve" value="" required/>
                     <br>
                 </div>
-                <div class="form-control">
+                <div class="form">
                     <label for="etablissementEleve">Etablissement:</label>
                     <select name="etablissementEleve" required >
                         <option>Choisir un établissement</option>
@@ -84,7 +87,7 @@
                     </select>
                     <br>
                 </div>
-                <div class="form-control"
+                <div class="form"
                      <label for="classeEleve">Classe:</label>
                     <input type="text" name="classeEleve" value="" required />
                     <br>
@@ -96,29 +99,31 @@
                 </div>
 
             </form>
-
+           <!-- Fin ajout etablissement -->  
+            
+            <!--Page AVS-->
             <form name="ajouterAVS" id="ajouterAVS" style="display: none;" action="index.php?do=ajouter&action=ajouterAVS" method="POST">
-                <div class="form-control">
+                <div class="form">
                     <label for="nomAVS">Nom:</label>
                     <input type="text" name="nomAVS" value="" required />
                     <br>
                 </div>
-                <div class="form-control">
+                <div class="form">
                     <label for="PrenomAVS">Prénom:</label>
                     <input type="text" name="PrenomAVS" value="" required />
                     <br>
                 </div>
-                <div class="form-control">
+                <div class="form">
                     <label for="dateNaissanceAVS">Date de naissance:</label>
                     <input type="date" name="dateNaissanceAVS" value="" required />
                     <br>
                 </div>
-                <div class="form-control">
+                <div class="form">
                     <label for="emailAVS">Email:</label>
                     <input type="email" name="emailAVS" value="" placeholder="xyz@gmail.com" required />
                     <br>
                 </div>
-                <div class="form-control">
+                <div class="form">
                     <label for="eleveAssigneAVS">Choisir un Elève:</label>
                     <select name="eleveAssigneAVS[]" id ="eleveAssigneAVS" class="form-control" multiple>
                         <?php
@@ -132,6 +137,7 @@
                     <br>
                     <br>
                 </div>
+                <!-- Fin ajout AVS --> 
                 <div class="col">
                     <div class="col"> 
                         <input type="submit" value="Sauvegarder" name="envoyerAVS" />
