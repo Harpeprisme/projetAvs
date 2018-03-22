80a8e39f1b468a506431ee7bbc392c122c56acaa
smailhamidi <35997316+smailhamidi@users.noreply.github.com>
2018-03-22 17:23:13 +0100

    V_historique des

--- vues/v_historique.php
+++ vues/v_historique.php
@@ -3,41 +3,54 @@
     <div class="row">
         <div class="col"></div>
         <div class="col">
-
-            <h2>Historique</h2>
+        
+                <h1>Historique</h1>
         </div>
+        <div class="col"></div>
+        </div>
+   
+ 
+
+
+    <div class="row">
+       
+        <div class="col">
+            <div class="searchbar"> <!--BARRE DE RECHERCHE-->
+                <div class="container-1"> <!--CONTAINER-->
+                    <input type="search" id="search" placeholder="Rechercher" style="width: 200px;" /> <!--INPUT-->
+                </div>
+
+            </div>
+
+        </div>
+         <div class="col"></div>
         <div class="col"></div>
     </div>
 
-    <div class="col row">
+    <div class="row">
         <!--
         
         Colonne numero 1
         
-        --> 
-              <div class="col-sm-1"></div>
-
-        <div class="col-sm-2">
-            
-            <p class="h3 " > Annee</p>
+        -->
+        
+        <div class="col">
            
-           <div class="row">
-                <input type="search" class="form-control " id="search" placeholder="Rechercher"  /> <!--INPUT-->
-            </div>
-
-            <select id="annee" class="col-ETAB" name="infoEtablissement" id="" multiple size="20" onClick="getAllAVS();">
-                <?php
-                foreach ($getAllAnnee as $annee) {
-                    echo'<option id=' . $annee['annee'] . '>' . $annee['annee'] . '</option>';
-                }
-                ?>
-            </select>
-
+               <p class="h3"> Annee
+               </p>
+        
+                <select id="annee" class="col-ETAB" name="infoEtablissement" id="" multiple size="20" onClick="getAllAVS();" style="width: 120px;">
+                    <?php
+                    foreach ($getAllAnnee as $annee) {
+                        echo'<option id=' . $annee['annee'] . '>' . $annee['annee'] . '</option>';
+                    }
+                    ?>
+                </select>
+       
         </div>
-        <div class="col-sm-1">   
-<!--            <span class="icon"><img src=http://constellations.pagesperso-orange.fr/Mes%20images/fl28.gif  width=60></span>-->
-        </div>
-
+          <div class="col">   
+          </div>
+            
         <div class="col"></div>
         <!--
        
@@ -45,36 +58,33 @@
         Colonne numero 2 AVS 
         
         --> 
-        <div class="col-sm-2" name="col-AVS" >
-
+        <div class="col" name="col-AVS" >
+        
             <p class="h3"> 
                 AVS
             </p>
-            <select class="col-ETAB" name="historiqueavs" id="historiqueavs" multiple size="24" onClick="getEleveAVSParAnnee()">
-                <?php
-                foreach ($getAVSParAnnee as $AVS) {
-                    if (isset($_GET['IdAVS']) && $AVS['id_avs'] == $_GET['IdAVS']) {
-                        echo'<option id=' . $AVS['id_avs'] . ' selected=selected value=' . $_GET['annee'] . '>' . $AVS['nom'] . ' ' . $AVS['prenom'] . '</option>';
-                    } else {
-                        echo'<option id=' . $AVS['id_avs'] . ' value=' . $_GET['annee'] . '>' . $AVS['nom'] . ' ' . $AVS['prenom'] . '</option>';
+                <select class="col-ETAB" name="historiqueavs" id="historiqueavs" multiple size="20" onClick="getEleveAVSParAnnee();" style="width: 120px;">
+                    <?php
+                    foreach ($getAVSParAnnee as $AVS) {
+                        if(isset($_GET['IdAVS']) && $AVS['id_avs'] == $_GET['IdAVS']){
+                                echo'<option id=' . $AVS['id_avs'] . ' selected=selected value='.$_GET['annee'].'>' . $AVS['nom'] . ' '.$AVS['prenom'].'</option>';
+                        } else{
+                                echo'<option id=' . $AVS['id_avs'] . ' value='.$_GET['annee'].'>' . $AVS['nom'] . ' '.$AVS['prenom'].'</option>';
+                            }
                     }
-                }
-                ?>
-            </select>
-
+                    ?>
+                </select>
+           
         </div>
         <!--
        
       
        
         -->
-
-        <div class="col-sm-1" id="ibAVS" name="col-AVS">
-
-            <legend>
-                <!--<span class="icon"><img src=http://constellations.pagesperso-orange.fr/Mes%20images/fl28.gif  width=60 alt="Responsive image"></span>-->
-
-            </legend>
+       
+        <div class="col" id="ibAVS" name="col-AVS">
+           
+                
         </div>
         <div class="col"></div>
         <!--
@@ -82,24 +92,24 @@
         Colonne numero 3
         
         -->
-        <div class="col-sm-2" id="ibAVS" name="col-AVS">
-
-            <p class="h3"> 
-                Eleve
-            </p>
-            <select class="col-ETAB" name="infoEtablissement" id="" multiple size="24">
-                <?php
-                foreach ($getEleveAVSParAnnee as $eleve) {
-                    echo'<option id=' . $eleve['id'] . '>' . $eleve['nom'] . ' ' . $eleve['prenom'] . '</option>';
-                }
-                ?>
-            </select>
-
+        <div class="col" id="ibAVS" name="col-AVS">
+           
+                <p class="h3"> 
+                    Eleve
+               </p>
+               <select class="col-ETAB" name="infoEtablissement" id="" multiple size="20" style="width: 120px;">
+                    <?php
+                    foreach ($getEleveAVSParAnnee as $eleve) {
+                        echo'<option id=' . $eleve['id'] . '>' . $eleve['nom'] . ' '.$eleve['prenom'].'</option>';
+                    }
+                    ?>
+                </select>
+          
         </div>
-        <div class="col"></div>
-        <div class="col"></div>
+         <div class="col"></div>
+          <div class="col"></div>
     </div>
-
+        
 </div>
 
 
